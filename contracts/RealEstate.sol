f3581d774df6404ea3bed21d2b58c6a4b0a24ee2
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-09 10:07:41 +0200

    Contracts add + json build

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,32 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity >=0.4.22 <0.9.0;
 
-contract RealEstate {
+// use of openzeppelin contracts
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract RealEstate is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Real Estate", "REAL") {}
+
+    // mint function
+    function mint(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    // total supply function
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+
 
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -1,10 +1,19 @@
-const { expect } = require('chai');
-const { ethers } = require('hardhat');
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-    return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
-describe('Escrow', () => {
+describe("Escrow", () => {
+  it("saves the addresses", async () => {
+    // Deploy real estate
+    const RealEstate = await ethers.getContractFactory("RealEstate");
+    const realEstate = await RealEstate.deploy();
 
-})
+    // Mint function
+    let transaction = await realEstate.mint(
+      "https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodYwmPekYgbnXGo4DFubJiLc2EB/1.json"
+    );
+  });
+});
