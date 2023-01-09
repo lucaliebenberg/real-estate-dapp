9263d17667b737080d99de3281673ef3997cdd12
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-09 11:22:16 +0200

    Escrow buyer and seller func added in tests (successful)

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -20,6 +20,11 @@ contract Escrow {
         _;
     }
 
+    modifier onlyBuyer(uint256 _nftID) {
+        require(msg.sender == buyer[_nftID], "Only buyer can call this method");
+        _;
+    }
+
     // Mappings
     mapping(uint256 => bool) public isListed;
     mapping(uint256 => uint256) public purchasePrice;
@@ -51,6 +56,19 @@ contract Escrow {
         purchasePrice[_nftID] = _purchasePrice;
         escrowAmount[_nftID] = _escrowAmount;
         buyer[_nftID] = _buyer;
+    }
+
+    // Put Under Contract (only buyer - payable escrow)
+    function depositEarnest(uint256 _nftID) public payable onlyBuyer(_nftID) {
+        require(msg.value >= escrowAmount[_nftID], "Amount needs to be more than escrow amount");
+    }
+
+    // neccessary to accept ether to the contract
+    receive() external payable {}
+
+    // Get contract balance
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 
 }
